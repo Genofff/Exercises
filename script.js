3ade3a95c98546274eba841b91d83ddb0b64d4d2
Genofff <114654769+Genofff@users.noreply.github.com>
2023-04-05 19:28:59 +0300

    exercise3

---
+++ script.js
@@ -0,0 +1,15 @@
+let firstNumber = +prompt("Введіть перше число:", []);
+
+let secondNumber = +prompt("Введіть друге число:", []);
+
+let result1 = firstNumber + secondNumber;
+alert("Сума чисел: " + result1);
+
+let result2 = Number(firstNumber) - Number(secondNumber);
+alert("Віднімання чисел: " + result2);
+
+let result3 = firstNumber * secondNumber;
+alert("Множення чисел: " + result3);
+
+let result4 = Number(firstNumber) / Number(secondNumber);
+alert("Ділення чисел: " + result4);
